2b647c5b84684bc0943edba089a55861864ae6d4
David G. Simmons <davidgs@influxdb.com>
2018-08-21 13:25:05 -0400

    Initial Commit
    a SenseAir K30 native Go driver.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# SenseAir_K30_go
+# SenseAir_k30_go
+
+## A Native Golang driver for the SenseAir K30 CO2 sensor.
+
+## Usage
+
+Create a K30 Object:
+
+    k30 := K30{}
+
+Initialize it with the proper device (for the Raspberry Pi 3, this works):
+
+    dev := "/dev/i2c-1"
+	r := k30.K30Init(dev)
+
+Returns 0 on success, -1 on failure.
+
+To read values:
+
+    reading := k30.K30ReadValue()
+	
+
+returned value will be the CO2 reading in Parts Per Million (PPM) or -1 on failure.
+
+It's a good idea to call
+
+    defer k30.Dev.Close()
+
+after the call to init so that the device will be closed after you're done with it. 
+
+## Error Handling
+
+There is none at this point. You have to handle them. but if the return is -1, it's likely that an error occured. It is very likely that subsequent readings will succeed, but is not guaranteed. I've left the print statements in at this point to help with error diagnosis.
---
+++ senseAirk30.go
@@ -0,0 +1,68 @@
+package SenseAir_K30_go
+
+import (
+	"fmt"
+	"golang.org/x/exp/io/i2c"
+)
+
+type K30 struct {
+	Dev     *i2c.Device
+	co2Read []byte
+}
+
+const (
+	CO2_ADDR = 0x68
+)
+
+//
+// Opens a file system handle to the I2C device
+//  and sets the device
+//
+func (device *K30) K30Init(channel string) int {
+	k30.co2Read = make([]byte, 4)
+	k30.co2Read[0] = 0x22
+	k30.co2Read[1] = 0x00
+	k30.co2Read[2] = 0x08
+	k30.co2Read[3] = 0x2A
+	response := make([]byte, 4)
+	var err error
+	device.Dev, err = i2c.Open(&i2c.Devfs{Dev: channel}, CO2_ADDR)
+	if err != nil {
+		panic(err)
+	}
+	return 0
+} /* K30Init() */
+
+//
+// Read the sensor register value
+// and translate into calibrated reading
+//
+
+func (k30 *K30) K30ReadValue() int {
+	result := make([]byte, 4)
+
+	err = k30.Dev.Write(k30.co2Read)
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	inBuf := []byte{0x00, 0x00, 0x00, 0x00}
+	time.Sleep(38 * time.Millisecond)
+	err = k30.Dev.Read(inBuf)
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	co2_value := 0
+	co2_value |= int(inBuf[1]) & int(0xFF)
+	co2_value = co2_value << 8
+	co2_value |= int(inBuf[2]) & int(0xFF)
+	sum := 0                                            //Checksum Byte
+	sum = int(inBuf[0]) + int(inBuf[1]) + int(inBuf[2]) //Byte addition utilizes overflow
+	if sum != int(inBuf[3]) {
+		fmt.Println("Checksum Error")
+		return -1
+	}
+	//fmt.Println("CO2: ", co2_value)
+	return co2Value
+}
